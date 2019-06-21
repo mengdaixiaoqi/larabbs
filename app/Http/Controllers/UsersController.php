03ae4ca57d316170662398754fc748a89552afcc
mengdaixiaoqi <1669003066@qq.com>
2019-06-21 15:12:47 +0800

    authorize

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -9,16 +9,21 @@ use App\Handlers\ImageUploadHandler;
 
 class UsersController extends Controller
 {
+    public function __construct(){
+        $this->middleware('auth',['except' => ['show']]);
+    }
     public function show(User $user){
 		return view('users.show',compact('user'));
 	}
 	//修改
 	public function edit(User $user){
+        $this->authorize('update', $user);
 		return view('users.edit',compact('user'));
 	}
 	//更新
 	public function update(UserRequest $request,ImageUploadHandler $uploader, User $user)
 	{
+        $this->authorize('update', $user);
         $data = $request->all();
 
         if($request->avatar){
--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use App\Policies\UserPolicy;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -13,7 +14,8 @@ class AuthServiceProvider extends ServiceProvider
      * @var array
      */
     protected $policies = [
-        // 'App\Model' => 'App\Policies\ModelPolicy',
+        'App\Model' => 'App\Policies\ModelPolicy',
+        \App\Models\User::class => \App\Policies\UserPolicy::class,
     ];
 
     /**
