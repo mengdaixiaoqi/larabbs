6d5a0d5f09484110d07ddcefd19f12b0055e97fe
mengdaixiaoqi <1669003066@qq.com>
2019-06-26 18:14:37 +0800

    fix edit bug

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,12 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Carbon\Carbon;
+use App\Observers\UserObserver;
+//use App\Observers\ReplyObserver;
+use App\Observers\TopicObserver;
+//use App\Models\Reply;
+use App\Models\Topic;
+use App\Models\User;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -26,5 +31,8 @@ class AppServiceProvider extends ServiceProvider
     {
         //
         \Carbon\Carbon::setlocale('zh');
+        User::observe(UserObserver::class);
+        //Reply::observe(ReplyObserver::class);
+        Topic::observe(TopicObserver::class);
     }
 }
