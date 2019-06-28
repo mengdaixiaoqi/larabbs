2cc3cc56b21796811e67ed85218c8849c8572c96
mengdaixiaoqi <1669003066@qq.com>
2019-06-28 11:02:41 +0800

    支持队列的邮件通知

--- app/Notifications/TopicReplied.php
+++ app/Notifications/TopicReplied.php
@@ -8,7 +8,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use App\Models\Reply;
 
-class TopicReplied extends Notification
+class TopicReplied extends Notification implements ShouldQueue
 {
     use Queueable;
 
@@ -33,8 +33,32 @@ class TopicReplied extends Notification
     public function via($notifiable)
     {
         // 开启通知的频道
-        return ['database'];
+        return ['database','mail'];
     }
+
+
+        /**
+         * Get the array representation of the notification.
+         *
+         * @param  mixed  $notifiable
+         * @return array
+         */
+         public function toDatabase($notifiable)
+         {
+             $topic = $this->reply->topic;
+             $link = $topic->link(['#reply' . $this->reply->id]);
+             // 存入数据库里的数据
+             return [
+                 'reply_id' => $this->reply->id,
+                 'reply_content' => $this->reply->content,
+                 'user_id' => $this->reply->user->id,
+                 'user_name' => $this->reply->user->name,
+                 'user_avatar' => $this->reply->user->avatar,
+                 'topic_link' => $link,
+                 'topic_id' => $topic->id,
+                 'topic_title' => $topic->title,
+             ];
+         }
 
     /**
      * Get the mail representation of the notification.
@@ -44,32 +68,11 @@ class TopicReplied extends Notification
      */
     public function toMail($notifiable)
     {
+        $url = $this->reply->topic->link(['#reply' . $this->reply->id]);
         return (new MailMessage)
-                    ->line('The introduction to the notification.')
-                    ->action('Notification Action', url('/'))
+                    ->line('你的话题有新回复！')
+                    ->action('查看回复', $url)
                     ->line('Thank you for using our application!');
     }
 
-    /**
-     * Get the array representation of the notification.
-     *
-     * @param  mixed  $notifiable
-     * @return array
-     */
-     public function toDatabase($notifiable)
-     {
-         $topic = $this->reply->topic;
-         $link = $topic->link(['#reply' . $this->reply->id]);
-         // 存入数据库里的数据
-         return [
-             'reply_id' => $this->reply->id,
-             'reply_content' => $this->reply->content,
-             'user_id' => $this->reply->user->id,
-             'user_name' => $this->reply->user->name,
-             'user_avatar' => $this->reply->user->avatar,
-             'topic_link' => $link,
-             'topic_id' => $topic->id,
-             'topic_title' => $topic->title,
-         ];
-     }
 }
